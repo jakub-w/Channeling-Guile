772ffb3bf365a523856571a9aeec61613fcef713
jakub-w <jakub-w@riseup.net>
2021-06-10 18:38:23 +0200

    Style and comment changes to cpp_dynwind_cpp_destroy

--- dynwind_exts.h
+++ dynwind_exts.h
@@ -1,3 +1,5 @@
+// Thanks to lloda @ #guile for some adjustments.
+
 #pragma once
 
 namespace guile_cpp_utils {
@@ -30,15 +32,17 @@ inline auto make_destructor(Arg& arg) {
 
 // Register ARGS objects to be destroyed on a non-local exit from the current
 // dynamic extent.
+// The objects will be destroyed in the reverse order they were passed in
+// (just like when calling this function multiple times).
 // Must be used inside of a dynamic wind context.
 template <typename... Args>
 inline void scm_dynwind_cpp_destroy(Args&... args) {
-  auto d = detail::make_destructor(args...);
-  using dest_t = detail::destructor_wrapper<decltype(d)>;
+  using dest_t =
+      decltype(detail::destructor_wrapper(detail::make_destructor(args...)));
 
   dest_t* dest = static_cast<dest_t*>(
       scm_gc_malloc_pointerless(sizeof(dest_t), "cpp-destructor"));
-  new (dest) dest_t(std::move(d));
+  new (dest) dest_t(detail::make_destructor(args...));
 
   scm_dynwind_unwind_handler(&dest_t::invoke, dest, scm_t_wind_flags(0));
 }
